c2d864a1ddb4ca2320f4f3fe891243878a8e9b6b
JalalJanKhan <jalaljankhan@outlook.com>
2024-09-30 01:23:33 +0500

    Build error resolved.

--- app/src/main/java/com/itsazcoding/permissionmanager/MainActivity.kt
+++ app/src/main/java/com/itsazcoding/permissionmanager/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.itsazcoding.permissionmanager
 
-import android.Manifest
+
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.itsazcoding.permissionmanager.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), IPermissionManager by PermissionManager() {
+class MainActivity : AppCompatActivity() {
     private lateinit var mBinding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -17,7 +16,7 @@ class MainActivity : AppCompatActivity(), IPermissionManager by PermissionManage
         enableEdgeToEdge()
         mBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(mBinding.root)
-        initialize(this)
+        //initialize(this)
 
         ViewCompat.setOnApplyWindowInsetsListener(mBinding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -31,55 +30,55 @@ class MainActivity : AppCompatActivity(), IPermissionManager by PermissionManage
     private fun clicks() {
         with(mBinding) {
             askCameraPermission.setOnClickListener {
-                requestRuntimePermissions(
-                    this@MainActivity,
-                    arrayOf(Manifest.permission.CAMERA)
-                )
+//                requestRuntimePermissions(
+//                    this@MainActivity,
+//                    arrayOf(Manifest.permission.CAMERA)
+//                )
 
-                onPermitted { granted ->
-                    if (granted) {
-                        Toast.makeText(this@MainActivity, "Permission Granted", Toast.LENGTH_SHORT)
-                            .show()
-                    } else {
-                        Toast.makeText(
-                            this@MainActivity,
-                            "Permission NOT Granted",
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
-                }
+//                onPermitted { granted ->
+//                    if (granted) {
+//                        Toast.makeText(this@MainActivity, "Permission Granted", Toast.LENGTH_SHORT)
+//                            .show()
+//                    } else {
+//                        Toast.makeText(
+//                            this@MainActivity,
+//                            "Permission NOT Granted",
+//                            Toast.LENGTH_SHORT
+//                        ).show()
+//                    }
+//                }
             }
 
             findCameraPermissionGranted.setOnClickListener {
-                if (permissionsGranted(this@MainActivity, Manifest.permission.CAMERA)) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Permission Already Granted",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                } else {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Permission NOT already granted",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+//                if (permissionsGranted(this@MainActivity, Manifest.permission.CAMERA)) {
+//                    Toast.makeText(
+//                        this@MainActivity,
+//                        "Permission Already Granted",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                } else {
+//                    Toast.makeText(
+//                        this@MainActivity,
+//                        "Permission NOT already granted",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                }
             }
 
             findCameraPermissionPermanentlyDenied.setOnClickListener {
-                if (permissionsPermanentlyDenied(this@MainActivity, Manifest.permission.CAMERA)) {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Permission permanently denied",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                } else {
-                    Toast.makeText(
-                        this@MainActivity,
-                        "Permission NOT permanently denied",
-                        Toast.LENGTH_SHORT
-                    ).show()
-                }
+//                if (permissionsPermanentlyDenied(this@MainActivity, Manifest.permission.CAMERA)) {
+//                    Toast.makeText(
+//                        this@MainActivity,
+//                        "Permission permanently denied",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                } else {
+//                    Toast.makeText(
+//                        this@MainActivity,
+//                        "Permission NOT permanently denied",
+//                        Toast.LENGTH_SHORT
+//                    ).show()
+//                }
             }
         }
     }
